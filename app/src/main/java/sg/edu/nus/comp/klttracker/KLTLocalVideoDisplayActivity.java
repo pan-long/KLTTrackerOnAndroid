ece63c9e8c1679f0a4183a0c591b3fe69c8c8089
Tang Ning <a0105529@soctf-imac-005.comp.nus.edu.sg>
2014-07-11 11:55:44 +0800

    added local video activity structure

--- app/src/main/java/sg/edu/nus/comp/klttracker/KLTLocalVideoDisplayActivity.java
+++ app/src/main/java/sg/edu/nus/comp/klttracker/KLTLocalVideoDisplayActivity.java
@@ -1,7 +1,41 @@
 package sg.edu.nus.comp.klttracker;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
 /**
  * Created by a0105529 on 7/11/14.
  */
-public class KLTLocalVideoDisplayActivity {
+public class KLTLocalVideoDisplayActivity extends Activity{
+
+
+    protected class DrawingView extends SurfaceView implements SurfaceHolder.Callback{
+        public DrawingView(Context context){
+            super(context);
+            getHolder().addCallback(this);
+        }
+
+        @Override
+        public void surfaceCreated(SurfaceHolder surfaceHolder) {
+
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i2, int i3) {
+
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+        }
+
+        @Override
+        public void onDraw(Canvas canvas){
+
+        }
+    }
 }
