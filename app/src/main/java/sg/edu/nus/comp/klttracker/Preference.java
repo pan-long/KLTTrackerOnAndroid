5c63572aae6098dac239f868f1514b31001b223d
Pan Long <aga.panlong@me.com>
2014-07-11 10:54:04 +0800

    added mode in Preference class

--- app/src/main/java/sg/edu/nus/comp/klttracker/Preference.java
+++ app/src/main/java/sg/edu/nus/comp/klttracker/Preference.java
@@ -6,6 +6,11 @@ import boofcv.struct.calib.IntrinsicParameters;
  * @author Peter Abeles
  */
 public class Preference {
+    // currently we support two modes, for camera and local video
+    static final int CAMERA_MODE = 0;
+    static final int VIDEO_MODE = 1;
+
+    int mode;
     int cameraId;
     int preview;
     int picture;
