bc86a0812141ec758a2eb499282e04bfa250a3de
Pan Long <aga.panlong@me.com>
2014-07-14 13:41:05 +0800

    added openGL support for local video mode

--- app/src/main/java/sg/edu/nus/comp/klttracker/KLTLocalVideoDisplayActivity.java
+++ app/src/main/java/sg/edu/nus/comp/klttracker/KLTLocalVideoDisplayActivity.java
@@ -11,6 +11,7 @@ import android.graphics.Bitmap;
 import android.media.MediaMetadataRetriever;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.GLU;
 import android.opengl.GLUtils;
 import android.os.Bundle;
 import android.view.Window;
@@ -18,6 +19,9 @@ import android.view.WindowManager;
 
 import org.ddogleg.struct.FastQueue;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,7 +46,7 @@ public class KLTLocalVideoDisplayActivity extends Activity {
     protected final Object lockGui = new Object();
     protected PointProcessing pointProcessing;
 
-    private boolean videoIsPaused;
+    private boolean videoIsPaused = false;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -50,12 +54,14 @@ public class KLTLocalVideoDisplayActivity extends Activity {
 
         GLSurfaceView DrawingView = new GLSurfaceView(this);
 
-        if (hasGLES20())
-            DrawingView.setEGLContextClientVersion(2);
+//        if (hasGLES20())
+//            DrawingView.setEGLContextClientVersion(2);
+        if (hasGLES10())
+            DrawingView.setEGLContextClientVersion(1);
         else
             dialogNoOpenGLES20();
 
-        DrawingView.setRenderer(new DrawingRenderer("/sdcard/Video/test.mp4", 50));
+        DrawingView.setRenderer(new DrawingRenderer("/sdcard/Video/test.mp4", 50000));
         setContentView(DrawingView);
     }
 
@@ -80,6 +86,12 @@ public class KLTLocalVideoDisplayActivity extends Activity {
         return configurationInfo.reqGlEsVersion >= 0x20000;
     }
 
+    private boolean hasGLES10() {
+        ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        ConfigurationInfo configurationInfo = activityManager.getDeviceConfigurationInfo();
+        return configurationInfo.reqGlEsVersion >= 0x10000;
+    }
+
     private void dialogNoOpenGLES20() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage("Your device does not support OpenGLES2.0!")
@@ -94,23 +106,41 @@ public class KLTLocalVideoDisplayActivity extends Activity {
     }
 
     protected class DrawingRenderer implements GLSurfaceView.Renderer {
+        private FloatBuffer vertexBuffer;
+        private float vertices[] = {
+                -1.0f, -1.0f,  0.0f,
+                -1.0f,  1.0f,  0.0f,
+                1.0f, -1.0f,  0.0f,
+                1.0f,  1.0f,  0.0f
+
+        };
+
         private MediaMetadataRetriever mediaMetadataRetriever;
         private int video_height;
         private int mHeight;
         private int video_width;
         private int mWidth;
+        private double scale;
+        private double tranX;
+        private double tranY;
         private byte[] storage;
         private long mOffset;
         private long previousTime;
         private long currentTime;
 
         public DrawingRenderer(String filePath, long offset) {
-            mediaMetadataRetriever = new MediaMetadataRetriever();
+//            mediaMetadataRetriever = new MediaMetadataRetriever();
+//
+//            mediaMetadataRetriever.setDataSource(filePath);
+//
+//            video_height = Integer.parseInt(mediaMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT));
+//            video_width = Integer.parseInt(mediaMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH));
 
-            mediaMetadataRetriever.setDataSource(filePath);
+            ByteBuffer vertexByteBuffer = ByteBuffer.allocateDirect(vertices.length * 4);
+            vertexByteBuffer.order(ByteOrder.nativeOrder());
 
-            video_height = Integer.parseInt(mediaMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT));
-            video_width = Integer.parseInt(mediaMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH));
+            vertexBuffer = vertexByteBuffer.asFloatBuffer();
+            vertexBuffer.position(0);
 
             this.mOffset = offset;
 
@@ -137,24 +167,47 @@ public class KLTLocalVideoDisplayActivity extends Activity {
         }
 
         @Override
-        public void onSurfaceCreated(GL10 notUsed, EGLConfig eglConfig) {
+        public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
         }
 
         @Override
-        public void onSurfaceChanged(GL10 notUsed, int width, int height) {
+        public void onSurfaceChanged(GL10 gl10, int width, int height) {
             mHeight = height;
             mWidth = width;
+
+            scale = Math.min(mHeight/(double)video_height, mWidth/(double)video_width);
+            tranX = (width-scale*video_width)/2;
+            tranY = (height-scale*video_height)/2;
+
+            gl10.glViewport(0, 0, width, height);
+            gl10.glMatrixMode(GL10.GL_PROJECTION);
+            gl10.glLoadIdentity();
+
+            GLU.gluPerspective(gl10, 45f, (float)width / (float)height, 0.1f, 10000f);
+
+            gl10.glMatrixMode(GL10.GL_MODELVIEW);
+            gl10.glLoadIdentity();
         }
 
         @Override
-        public void onDrawFrame(GL10 notUsed) {
+        public void onDrawFrame(GL10 gl10) {
             if (currentTime > previousTime) {
-                Bitmap frame = mediaMetadataRetriever.getFrameAtTime(mOffset);
-                storage = ConvertBitmap.declareStorage(frame, storage);
-                ImageUInt8 gray = new ImageUInt8(video_width, video_height);
-                ConvertBitmap.bitmapToGray(frame, gray, storage);
+//                Bitmap frame = mediaMetadataRetriever.getFrameAtTime(mOffset);
+//                storage = ConvertBitmap.declareStorage(frame, storage);
+//                ImageUInt8 gray = new ImageUInt8(video_width, video_height);
+//                ConvertBitmap.bitmapToGray(frame, gray, storage);
+//
+//                pointProcessing.process(gray);
 
-                pointProcessing.process(gray);
+                gl10.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+                gl10.glLoadIdentity();
+                gl10.glTranslatef(0f, 0f, -5f);
+
+                gl10.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+                gl10.glColor4f(0f, 1f, 0f, 0.5f);
+                gl10.glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer);
+                gl10.glDrawArrays(GL10.GL_TRIANGLE_STRIP, 0, vertices.length/3);
+                gl10.glDisableClientState(GL10.GL_VERTEX_ARRAY);
 
                 previousTime = currentTime;
             }
