ccc448c3e61ee251eea083f052c06798c5e1ed93
Pan Long <aga.panlong@me.com>
2014-07-14 12:12:29 +0800

    removed try and catch for setDataSource of MediaMetadataRetriever

--- app/src/main/java/sg/edu/nus/comp/klttracker/KLTLocalVideoDisplayActivity.java
+++ app/src/main/java/sg/edu/nus/comp/klttracker/KLTLocalVideoDisplayActivity.java
@@ -107,11 +107,7 @@ public class KLTLocalVideoDisplayActivity extends Activity {
         public DrawingRenderer(String filename, long offset) {
             mediaMetadataRetriever = new MediaMetadataRetriever();
 
-            try {
-                mediaMetadataRetriever.setDataSource(filename);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            mediaMetadataRetriever.setDataSource(filename);
 
             video_height = Integer.parseInt(mediaMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT));
             video_width = Integer.parseInt(mediaMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH));
