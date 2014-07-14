9fdd764af5d98bb7fed55f1480196d4a7d2d3654
Pan Long <aga.panlong@me.com>
2014-07-14 10:30:46 +0800

    changed offset and time to long and added new thread

--- app/src/main/java/sg/edu/nus/comp/klttracker/KLTLocalVideoDisplayActivity.java
+++ app/src/main/java/sg/edu/nus/comp/klttracker/KLTLocalVideoDisplayActivity.java
@@ -35,8 +35,10 @@ import georegression.struct.point.Point2D_F64;
 /**
  * Created by a0105529 on 7/11/14.
  */
-public class KLTLocalVideoDisplayActivity extends Activity{
+public class KLTLocalVideoDisplayActivity extends Activity {
     protected final Object lockGui = new Object();
+
+    private boolean videoIsPaused;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -95,11 +97,11 @@ public class KLTLocalVideoDisplayActivity extends Activity{
         private int mHeight;
         private int video_width;
         private int mWidth;
-        private int offset;
-        private int previousTime;
-        private int currentTime;
+        private long offset;
+        private long previousTime;
+        private long currentTime;
 
-        public DrawingRenderer(String filename, int offset) {
+        public DrawingRenderer(String filename, long offset) {
             mediaMetadataRetriever = new MediaMetadataRetriever();
             mediaMetadataRetriever.setDataSource(filename);
             video_height = Integer.parseInt(mediaMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT));
@@ -108,6 +110,14 @@ public class KLTLocalVideoDisplayActivity extends Activity{
             this.offset = offset;
 
             previousTime = currentTime = 0;
+
+            Thread thread = new Thread() {
+                @Override
+                public void run() {
+                    
+                }
+            };
+            thread.start();
         }
 
         @Override
@@ -140,56 +150,56 @@ public class KLTLocalVideoDisplayActivity extends Activity{
         List<PointTrack> inactive = new ArrayList<PointTrack>();
 
         // storage for data structures that are displayed in the GUI
-        FastQueue<Point2D_F64> trackSrc = new FastQueue<Point2D_F64>(Point2D_F64.class,true);
-        FastQueue<Point2D_F64> trackDst = new FastQueue<Point2D_F64>(Point2D_F64.class,true);
-        FastQueue<Point2D_F64> trackSpawn = new FastQueue<Point2D_F64>(Point2D_F64.class,true);
+        FastQueue<Point2D_F64> trackSrc = new FastQueue<Point2D_F64>(Point2D_F64.class, true);
+        FastQueue<Point2D_F64> trackDst = new FastQueue<Point2D_F64>(Point2D_F64.class, true);
+        FastQueue<Point2D_F64> trackSpawn = new FastQueue<Point2D_F64>(Point2D_F64.class, true);
 
 
-        public PointProcessing( PointTracker<ImageUInt8> tracker ) {
+        public PointProcessing(PointTracker<ImageUInt8> tracker) {
             //super(boofcv.struct.image.ImageType.single(boofcv.struct.image.ImageUInt8.class));
             //super(ImageType.ms(3, ImageUInt8.class));
             this.tracker = tracker;
         }
 
-        protected void process(MultiSpectral<ImageUInt8> color) {
-            ImageUInt8 gray = new ImageUInt8(imageWidth, imageHeight);
-            GConvertImage.convert(color, gray);
+        protected void process(ImageUInt8 gray) {
+//            ImageUInt8 gray = new ImageUInt8(imageWidth, imageHeight);
+//            GConvertImage.convert(color, gray);
             tracker.process(gray);
 
             // drop tracks which are no longer being used
             inactive.clear();
             tracker.getInactiveTracks(inactive);
-            for( int i = 0; i < inactive.size(); i++ ) {
+            for (int i = 0; i < inactive.size(); i++) {
                 PointTrack t = inactive.get(i);
                 TrackInfo info = t.getCookie();
-                if( tick - info.lastActive > 2 ) {
+                if (tick - info.lastActive > 2) {
                     tracker.dropTrack(t);
                 }
             }
 
             active.clear();
             tracker.getActiveTracks(active);
-            for( int i = 0; i < active.size(); i++ ) {
+            for (int i = 0; i < active.size(); i++) {
                 PointTrack t = active.get(i);
                 TrackInfo info = t.getCookie();
                 info.lastActive = tick;
             }
 
             spawned.clear();
-            if( active.size() < 50 )  {
+            if (active.size() < 50) {
                 tracker.spawnTracks();
 
                 // update the track's initial position
-                for( int i = 0; i < active.size(); i++ ) {
+                for (int i = 0; i < active.size(); i++) {
                     PointTrack t = active.get(i);
                     TrackInfo info = t.getCookie();
                     info.spawn.set(t);
                 }
 
                 tracker.getNewTracks(spawned);
-                for( int i = 0; i < spawned.size(); i++ ) {
+                for (int i = 0; i < spawned.size(); i++) {
                     PointTrack t = spawned.get(i);
-                    if( t.cookie == null ) {
+                    if (t.cookie == null) {
                         t.cookie = new TrackInfo();
                     }
                     TrackInfo info = t.getCookie();
@@ -198,15 +208,15 @@ public class KLTLocalVideoDisplayActivity extends Activity{
                 }
             }
 
-            synchronized ( lockGui ) {
+            synchronized (lockGui) {
                 //ConvertBitmap.grayToBitmap(gray,bitmap,storage);
-                ConvertBitmap.multiToBitmap(color, bitmap, storage);
+//                ConvertBitmap.multiToBitmap(color, bitmap, storage);
 
                 trackSrc.reset();
                 trackDst.reset();
                 trackSpawn.reset();
 
-                for( int i = 0; i < active.size(); i++ ) {
+                for (int i = 0; i < active.size(); i++) {
                     PointTrack t = active.get(i);
                     TrackInfo info = t.getCookie();
                     Point2D_F64 s = info.spawn;
@@ -216,7 +226,7 @@ public class KLTLocalVideoDisplayActivity extends Activity{
                     trackDst.grow().set(p);
                 }
 
-                for( int i = 0; i < spawned.size(); i++ ) {
+                for (int i = 0; i < spawned.size(); i++) {
                     Point2D_F64 p = spawned.get(i);
                     trackSpawn.grow().set(p);
                 }
