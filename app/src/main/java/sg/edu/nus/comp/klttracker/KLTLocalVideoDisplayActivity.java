c380318937703df6eff63021b852f50afee74501
Pan Long <aga.panlong@me.com>
2014-07-14 12:00:47 +0800

    testing path

--- app/src/main/java/sg/edu/nus/comp/klttracker/KLTLocalVideoDisplayActivity.java
+++ app/src/main/java/sg/edu/nus/comp/klttracker/KLTLocalVideoDisplayActivity.java
@@ -55,7 +55,7 @@ public class KLTLocalVideoDisplayActivity extends Activity {
         else
             dialogNoOpenGLES20();
 
-        DrawingView.setRenderer(new DrawingRenderer("/Video/test.mp4", 50));
+        DrawingView.setRenderer(new DrawingRenderer("/mnt/sdcard/Video/test.mp4", 50));
         setContentView(DrawingView);
     }
 
